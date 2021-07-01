e6bfe40fcdbffc824145231ecb661776a8786b22
youngbloood <studio.source.net>
2021-07-01 14:53:29 +0800

    error

--- error.go
+++ error.go
@@ -1,19 +1,31 @@
 package gopool
 
-import "errors"
-
-type HookError interface {
-	HookErr(error)
-}
-
-var hook HookError
-
-func SetHook(hook HookError) {
-	hook = hook
-}
+import (
+	"errors"
+	"log"
+)
 
 var (
 	ErrTimeOut = errors.New("send timeout!")
 	ErrNotIdle = errors.New("no idle goroutine!")
 	ErrStoped  = errors.New("gopool stoped!")
+	errHandle  = errors.New("no function to handle the value v!")
+
+	hook HookError = &_hookError{}
 )
+
+type HookError interface {
+	HookErr(error)
+}
+
+func SetHook(he HookError) {
+	hook = he
+}
+
+type _hookError struct{}
+
+func (_hookError) HookErr(err error) {
+	if err != nil {
+		log.Printf("[HookError]:%+v\n", err)
+	}
+}
